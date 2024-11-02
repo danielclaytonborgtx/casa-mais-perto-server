ab26a544cd07ecb2aecf3f3a8719163729627904
Paulo André <pauloandre0011@gmail.com>
2024-11-02 18:37:44 -0300

    Add new schema

--- prisma/migrations/20241102212706_init_data_base/migration.sql
+++ prisma/migrations/20241102212706_init_data_base/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "username" TEXT NOT NULL,
-    "password" TEXT
+    "password" TEXT NOT NULL
 );
 
 -- CreateTable
--- src/server.ts
+++ src/server.ts
@@ -108,64 +108,64 @@ server.post(
 );
 
 // Rota de login
-// server.post(
-//   "/login",
-//   async (
-//     request: FastifyRequest<{ Body: LoginRequest }>,
-//     reply: FastifyReply
-//   ) => {
-//     const { error } = loginSchema.validate(request.body);
+server.post(
+  "/session",
+  async (
+    request: FastifyRequest<{ Body: LoginRequest }>,
+    reply: FastifyReply
+  ) => {
+    const { error } = loginSchema.validate(request.body);
 
-//     if (error) {
-//       return reply.status(400).send({ error: error.details[0].message });
-//     }
+    if (error) {
+      return reply.status(400).send({ error: error.details[0].message });
+    }
 
-//     const { username, password } = request.body;
+    const { username, password } = request.body;
 
-//     try {
-//       const user = await prisma.user.findUnique({ where: { username } });
+    try {
+      const user = await prisma.user.findUnique({ where: { username } });
 
-//       if (!user || !(await bcrypt.compare(password, user.password))) {
-//         return reply
-//           .status(401)
-//           .send({ error: "Invalid username or password" });
-//       }
+      if (!user || !bcrypt.compare(password, user.password)) {
+        return reply
+          .status(401)
+          .send({ error: "Invalid username or password" });
+      }
 
-//       return reply.send({ message: "Login successful", user });
-//     } catch (error) {
-//       console.error("Error during login:", error);
-//       return reply.status(500).send({ error: "Failed to login" });
-//     }
-//   }
-// );
+      return reply.send({ message: "Login successful", user });
+    } catch (error) {
+      console.error("Error during login:", error);
+      return reply.status(500).send({ error: "Failed to login" });
+    }
+  }
+);
 
 // Rota de buscar usuário por ID
-// server.get(
-//   "/users/:id",
-//   async (
-//     request: FastifyRequest<{ Params: { id: string } }>,
-//     reply: FastifyReply
-//   ) => {
-//     const userId = Number(request.params.id);
+server.get(
+  "/users/:id",
+  async (
+    request: FastifyRequest<{ Params: { id: string } }>,
+    reply: FastifyReply
+  ) => {
+    const userId = Number(request.params.id);
 
-//     if (isNaN(userId)) {
-//       return reply.status(400).send({ error: "Invalid userId" });
-//     }
+    if (isNaN(userId)) {
+      return reply.status(400).send({ error: "Invalid userId" });
+    }
 
-//     try {
-//       const user = await prisma.user.findUnique({ where: { id: userId } });
+    try {
+      const user = await prisma.user.findUnique({ where: { id: userId } });
 
-//       if (!user) {
-//         return reply.status(404).send({ error: "Usuário não encontrado" });
-//       }
+      if (!user) {
+        return reply.status(404).send({ error: "Usuário não encontrado" });
+      }
 
-//       return reply.send(user);
-//     } catch (error) {
-//       console.error(error);
-//       return reply.status(500).send({ error: "Failed to fetch user" });
-//     }
-//   }
-// );
+      return reply.send(user);
+    } catch (error) {
+      console.error(error);
+      return reply.status(500).send({ error: "Failed to fetch user" });
+    }
+  }
+);
 
 // Rota de registro de imóveis
 server.post(
@@ -214,197 +214,194 @@ server.post(
 );
 
 // Rota para listar imóveis
-// server.get("/imoveis", async (request: FastifyRequest, reply: FastifyReply) => {
-//   try {
-//     const imoveis = await prisma.imovel.findMany({
-//       include: { imagens: true },
-//     });
-//     return reply.send(imoveis);
-//   } catch (error) {
-//     console.error("Erro ao buscar imóveis:", error);
-//     return reply.status(500).send({ error: "Falha ao buscar imóveis" });
-//   }
-// });
+server.get(
+  "/property",
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const imoveis = await prisma.property.findMany({
+        include: { images: true },
+      });
+      return reply.send(imoveis);
+    } catch (error) {
+      console.error("Erro ao buscar imóveis:", error);
+      return reply.status(500).send({ error: "Falha ao buscar imóveis" });
+    }
+  }
+);
 
 // Rota para listar imóveis de um usuário específico
-// server.get(
-//   "/imoveis/user",
-//   async (
-//     request: FastifyRequest<{ Querystring: { userId: string } }>,
-//     reply: FastifyReply
-//   ) => {
-//     const { userId } = request.query;
+server.get(
+  "/property/user",
+  async (
+    request: FastifyRequest<{ Querystring: { userId: string } }>,
+    reply: FastifyReply
+  ) => {
+    const { userId } = request.query;
 
-//     const numericUserId = Number(userId); // Converte userId para número
+    const numericUserId = Number(userId); // Converte userId para número
 
-//     if (isNaN(numericUserId)) {
-//       return reply
-//         .status(400)
-//         .send({ error: "UserId é obrigatório e deve ser um número" });
-//     }
+    if (isNaN(numericUserId)) {
+      return reply
+        .status(400)
+        .send({ error: "UserId é obrigatório e deve ser um número" });
+    }
 
-//     try {
-//       const imoveis = await prisma.imovel.findMany({
-//         where: { userId: numericUserId },
-//         include: { imagens: true },
-//       });
+    try {
+      const imoveis = await prisma.property.findMany({
+        where: { userId: numericUserId },
+        include: { images: true },
+      });
 
-//       if (imoveis.length === 0) {
-//         return reply
-//           .status(404)
-//           .send({ message: "Nenhum imóvel encontrado para este usuário" });
-//       }
+      if (imoveis.length === 0) {
+        return reply
+          .status(404)
+          .send({ message: "Nenhum imóvel encontrado para este usuário" });
+      }
 
-//       return reply.send(imoveis);
-//     } catch (error) {
-//       console.error("Erro ao buscar imóveis do usuário:", error);
-//       return reply.status(500).send({ error: "Falha ao buscar imóveis" });
-//     }
-//   }
-// );
+      return reply.send(imoveis);
+    } catch (error) {
+      console.error("Erro ao buscar imóveis do usuário:", error);
+      return reply.status(500).send({ error: "Falha ao buscar imóveis" });
+    }
+  }
+);
 
 // Rota para obter detalhes de um imóvel específico
-// server.get(
-//   "/imoveis/:id",
-//   async (
-//     request: FastifyRequest<{ Params: { id: string } }>,
-//     reply: FastifyReply
-//   ) => {
-//     const { id } = request.params;
-//     console.log("ID recebido:", id); // Log para verificar o ID
-//     console.log("Fetching property with ID:", id);
+server.get(
+  "/imoveis/:id",
+  async (
+    request: FastifyRequest<{ Params: { id: string } }>,
+    reply: FastifyReply
+  ) => {
+    const { id } = request.params;
+    console.log("ID recebido:", id); // Log para verificar o ID
+    console.log("Fetching property with ID:", id);
 
-//     const propertyId = Number(id);
-//     if (isNaN(propertyId)) {
-//       return reply.status(400).send({ error: "ID deve ser um número válido" });
-//     }
+    const propertyId = Number(id);
+    if (isNaN(propertyId)) {
+      return reply.status(400).send({ error: "ID deve ser um número válido" });
+    }
 
-//     try {
-//       console.log("Buscando imóvel com ID:", propertyId); // Log do ID que será buscado
-//       const imovel = await prisma.imovel.findUnique({
-//         where: {
-//           id: propertyId,
-//         },
-//         include: { imagens: true },
-//       });
+    try {
+      console.log("Buscando imóvel com ID:", propertyId); // Log do ID que será buscado
+      const property = await prisma.property.findUnique({
+        where: {
+          id: propertyId,
+        },
+        include: { images: true },
+      });
 
-//       if (!imovel) {
-//         return reply.status(404).send({ error: "Imóvel não encontrado" });
-//       }
+      if (!property) {
+        return reply.status(404).send({ error: "Imóvel não encontrado" });
+      }
 
-//       return reply.send(imovel);
-//     } catch (error) {
-//       console.error("Erro ao buscar imóvel:", error);
-//       return reply.status(500).send({ error: "Falha ao buscar imóvel" });
-//     }
-//   }
-// );
+      return reply.send(property);
+    } catch (error) {
+      console.error("Erro ao buscar imóvel:", error);
+      return reply.status(500).send({ error: "Falha ao buscar imóvel" });
+    }
+  }
+);
 
 // Rota para editar um imóvel
-// server.put(
-//   "/imoveis/:id",
-//   async (
-//     request: FastifyRequest<{ Params: { id: string }; Body: PropertyRequest }>,
-//     reply: FastifyReply
-//   ) => {
-//     const { id } = request.params;
-//     const { titulo, descricao, imagens, latitude, longitude, userId } =
-//       request.body; // Inclua userId aqui
+server.put(
+  "/property/:id",
+  async (
+    request: FastifyRequest<{ Params: { id: string }; Body: PropertyRequest }>,
+    reply: FastifyReply
+  ) => {
+    const { id } = request.params;
+    const { title, description, latitude, longitude, userId } = request.body; // Inclua userId aqui
 
-//     // Validação do ID do imóvel
-//     const propertyId = Number(id);
-//     if (isNaN(propertyId)) {
-//       return reply
-//         .status(400)
-//         .send({ error: "ID do imóvel deve ser um número" });
-//     }
+    // Validação do ID do imóvel
+    const propertyId = Number(id);
+    if (isNaN(propertyId)) {
+      return reply
+        .status(400)
+        .send({ error: "ID do imóvel deve ser um número" });
+    }
 
-//     // Validação dos dados do imóvel
-//     const { error } = propertySchema.validate({
-//       titulo,
-//       descricao,
-//       imagens,
-//       latitude,
-//       longitude,
-//       userId,
-//     }); // Inclua userId na validação
-//     if (error) {
-//       return reply.status(400).send({ error: error.details[0].message });
-//     }
+    // Validação dos dados do imóvel
+    const { error } = propertySchema.validate({
+      title,
+      description,
+      latitude,
+      longitude,
+      userId,
+    }); // Inclua userId na validação
+    if (error) {
+      return reply.status(400).send({ error: error.details[0].message });
+    }
 
-//     try {
-//       // Verifica se o imóvel existe antes de tentar editar
-//       const existingProperty = await prisma.imovel.findUnique({
-//         where: { id: propertyId },
-//       });
-//       if (!existingProperty) {
-//         return reply.status(404).send({ error: "Imóvel não encontrado" });
-//       }
+    try {
+      // Verifica se o imóvel existe antes de tentar editar
+      const existingProperty = await prisma.property.findUnique({
+        where: { id: propertyId },
+      });
+      if (!existingProperty) {
+        return reply.status(404).send({ error: "Imóvel não encontrado" });
+      }
 
-//       // Atualiza o imóvel
-//       const imovel = await prisma.imovel.update({
-//         where: { id: propertyId },
-//         data: {
-//           titulo,
-//           descricao,
-//           latitude,
-//           longitude,
-//           imagens: {
-//             deleteMany: {}, // Remove as imagens antigas
-//             create: imagens.map((url: string) => ({ url })), // Adiciona as novas imagens
-//           },
-//         },
-//         include: { imagens: true },
-//       });
+      // Atualiza o imóvel
+      const property = await prisma.property.update({
+        where: { id: propertyId },
+        data: {
+          title,
+          description,
+          latitude,
+          longitude,
+        },
+        include: { images: true },
+      });
 
-//       return reply.status(200).send(imovel);
-//     } catch (error) {
-//       console.error("Erro ao editar imóvel:", error);
-//       return reply.status(500).send({ error: "Falha ao editar imóvel" });
-//     }
-//   }
-// );
+      return reply.status(200).send(property);
+    } catch (error) {
+      console.error("Erro ao editar imóvel:", error);
+      return reply.status(500).send({ error: "Falha ao editar imóvel" });
+    }
+  }
+);
 
 // Rota para deletar um imóvel
-// server.delete(
-//   "/imoveis/:id",
-//   async (
-//     request: FastifyRequest<{ Params: { id: string } }>,
-//     reply: FastifyReply
-//   ) => {
-//     const { id } = request.params;
+server.delete(
+  "/property/:id",
+  async (
+    request: FastifyRequest<{ Params: { id: string } }>,
+    reply: FastifyReply
+  ) => {
+    const { id } = request.params;
 
-//     const propertyId = Number(id);
-//     if (isNaN(propertyId)) {
-//       return reply
-//         .status(400)
-//         .send({ error: "ID do imóvel deve ser um número válido" });
-//     }
+    const propertyId = Number(id);
+    if (isNaN(propertyId)) {
+      return reply
+        .status(400)
+        .send({ error: "ID do imóvel deve ser um número válido" });
+    }
 
-//     try {
-//       // Verifica se o imóvel existe antes de tentar deletar
-//       const existingProperty = await prisma.imovel.findUnique({
-//         where: { id: propertyId },
-//         include: { imagens: true }, // Inclui as imagens para verificar se existem
-//       });
+    try {
+      // Verifica se o imóvel existe antes de tentar deletar
+      const existingProperty = await prisma.property.findUnique({
+        where: { id: propertyId },
+        include: { images: true }, // Inclui as imagens para verificar se existem
+      });
 
-//       if (!existingProperty) {
-//         return reply.status(404).send({ error: "Imóvel não encontrado" });
-//       }
+      if (!existingProperty) {
+        return reply.status(404).send({ error: "Imóvel não encontrado" });
+      }
 
-//       // Deleta todas as imagens associadas ao imóvel
-//       await prisma.imagem.deleteMany({ where: { imovelId: propertyId } });
+      // Deleta todas as imagens associadas ao imóvel
+      await prisma.image.deleteMany({ where: { propertyId: propertyId } });
 
-//       // Deleta o imóvel
-//       await prisma.imovel.delete({ where: { id: propertyId } });
+      // Deleta o imóvel
+      await prisma.image.delete({ where: { id: propertyId } });
 
-//       return reply.status(200).send({ message: "Imóvel deletado com sucesso" });
-//     } catch (error) {
-//       console.error("Erro ao deletar imóvel:", error);
-//       return reply.status(500).send({ error: "Falha ao deletar imóvel" });
-//     }
-//   }
-// );
+      return reply.status(200).send({ message: "Imóvel deletado com sucesso" });
+    } catch (error) {
+      console.error("Erro ao deletar imóvel:", error);
+      return reply.status(500).send({ error: "Falha ao deletar imóvel" });
+    }
+  }
+);
 
 // Iniciar o servidor
 server.listen({ port: 3333, host: "0.0.0.0" }, (err) => {
